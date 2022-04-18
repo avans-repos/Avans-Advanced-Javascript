bea9c7e62c1c0bccb9fd2151cc04df12d5568e15
Job <LordChunk@users.noreply.github.com>
2022-04-18 23:52:50 +0200

    Added AngularFire & added Firebase deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'avans-adweb',
+    appId: '1:952710083615:web:9168ad3e0affb6a2d3112a',
+    storageBucket: 'avans-adweb.appspot.com',
+    apiKey: 'AIzaSyAO_FppM8n7M1Ykf0IleKHlv8xvkeVckAY',
+    authDomain: 'avans-adweb.firebaseapp.com',
+    messagingSenderId: '952710083615',
+  },
   production: false
 };
 
