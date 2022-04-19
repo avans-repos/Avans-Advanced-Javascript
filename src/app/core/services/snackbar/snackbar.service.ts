ccbfdcee29b232d7cf452fd30ccc4882818b5672
Job <LordChunk@users.noreply.github.com>
2022-04-20 00:06:42 +0200

    Scaffolded snackbar service

---
+++ src/app/core/services/snackbar/snackbar.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+
+/**
+ * Service to messages to be shown in the snackbar.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackbarService {
+
+  constructor() { }
+}
