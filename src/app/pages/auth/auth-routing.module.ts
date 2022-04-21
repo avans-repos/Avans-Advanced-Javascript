2f8794b366ff4de71d23660328cbe83f82545150
Job <LordChunk@users.noreply.github.com>
2022-04-21 13:51:00 +0200

    Removed auth component and added login component

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,8 +1,9 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+
+const routes: Routes = [{ path: 'login', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
