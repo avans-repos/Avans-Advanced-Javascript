d3d7a56cf9731edd27fbf6068b96aa6157120732
Job <LordChunk@users.noreply.github.com>
2022-04-22 15:37:41 +0200

    Added some basic elements for booklet creation

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,17 +1,23 @@
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard.component';
+import { DashboardComponent } from './index/dashboard.component';
+import { CreateComponent } from './create/create.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    CreateComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class DashboardModule { }
