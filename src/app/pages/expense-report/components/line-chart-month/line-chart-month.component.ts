78aa0f8553c9172a262c2029a721c7bb07db19a7
Job <LordChunk@users.noreply.github.com>
2022-06-22 21:57:00 +0200

    Added line graph

--- src/app/pages/expense-report/components/line-chart-month/line-chart-month.component.ts
+++ src/app/pages/expense-report/components/line-chart-month/line-chart-month.component.ts
@@ -1,57 +1,69 @@
-import { Component, Input } from '@angular/core';
-import { Transaction } from '@angular/fire/firestore';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Required } from 'src/app/core/decorators/required-input';
+import { Transaction } from 'src/app/core/models/transaction';
 
 @Component({
   selector: 'app-line-chart-month',
   templateUrl: './line-chart-month.component.html',
   styleUrls: ['./line-chart-month.component.scss'],
 })
-export class LineChartMonthComponent {
-  selectedMonth: BehaviorSubject<Date> = new BehaviorSubject(new Date());
-
+export class LineChartMonthComponent implements OnInit {
   totalBalance: Observable<number> = of(0);
 
-  view: [number, number] = [700, 300];
+  view: [number, number] = [700, 400];
 
-  @Input() @Required public transactions!: Observable<Transaction[]>;
+  @Input() @Required public transactions: Observable<Transaction[]> = of([]);
 
-  public results = [
-    {
-      name: 'Germany',
-      series: [
-        {
-          name: '2010',
-          value: 7300000,
-          min: 7000000,
-          max: 7600000,
-        },
-        {
-          name: '2011',
-          value: 8940000,
-          min: 8840000,
-          max: 9300000,
-        },
-      ],
-    },
+  public results!: object[];
 
-    {
-      name: 'USA',
-      series: [
-        {
-          name: '2010',
-          value: 7870000,
-          min: 7800000,
-          max: 7950000,
-        },
-        {
-          name: '2011',
-          value: 8270000,
-          min: 8170000,
-          max: 8300000,
-        },
-      ],
-    },
-  ];
+  ngOnInit() {
+    this.transactions.subscribe((transactions) => {
+      this.results = this.fillGraph(transactions);
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private calculateMonth(dateValues: Map<number, number>) {
+    const series: Object[] = [];
+    let start = 0;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [date, value] of dateValues) {
+      start += value;
+      series.push({
+        name: date,
+        value: start,
+      });
+    }
+
+    return series;
+  }
+
+  private fillGraph(transactions: Transaction[]): any {
+    if (transactions.length === 0) return [];
+    const dateValues = this.daysInMonth(transactions[0].date.toDate());
+
+    transactions.forEach((transaction) => {
+      const date = transaction.date.toDate().getDate();
+      dateValues.set(date, (dateValues.get(date) ?? 0) + transaction.amount);
+    });
+
+    return [{
+      name: 'Balance',
+      series: this.calculateMonth(dateValues),
+    }];
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private daysInMonth(date: Date) {
+    const mapData = new Map<number, number>();
+
+    const month = new Date(date.getFullYear(), date.getMonth(), 1);
+    const nextMonth = new Date(month.getFullYear(), month.getMonth() + 1, 1);
+    for (let d = month; d < nextMonth; d.setDate(d.getDate() + 1)) {
+      mapData.set(d.getDate(), 0);
+    }
+
+    return mapData;
+  }
 }
